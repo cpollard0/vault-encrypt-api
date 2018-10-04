d527cfff85e1013d513ea830b2a5a42ca9e2439b
Chris Pollard <cpollard@promnetwork.com>
2018-10-04 10:46:11 -0400

    Cleanup

--- vault_encrypt_lambda.py
+++ vault_encrypt_lambda.py
@@ -13,16 +13,13 @@ LOGGER.setLevel(logging.INFO)
 DEBUG_MODE = True
 if DEBUG_MODE:
     LOGGER.setLevel(logging.DEBUG)
+
 ansible.constants.DEFAULT_LOCAL_TMP = '/tmp/ansible'
 ansible.constants.DEFAULT_REMOTE_TMP = '/tmp/ansible'
 ansible.local_tmp = '/tmp/ansible'
-ANSIBLE_VER = float('.'.join(ansible.__version__.split('.')[:2]))
+
 # Set directories or ansible tries to set on it's own, which breaks in lambda
 def make_secret(secret):
-    """ creates vault secret based on the version of ansible being ran"""
-    if ANSIBLE_VER < 2.4:
-        return secret
-
     from ansible.constants import DEFAULT_VAULT_ID_MATCH
     from ansible.parsing.vault import VaultSecret
     return [(DEFAULT_VAULT_ID_MATCH, VaultSecret(secret))]
