54d42277f9eb954f49ec0d3bdc39673a83419567
Chris Pollard <cpollard@promnetwork.com>
2018-10-09 10:15:30 -0400

    Update to get an SSM param as encryption key

--- vault_encrypt_lambda.py
+++ vault_encrypt_lambda.py
@@ -17,6 +17,7 @@ if DEBUG_MODE:
 ansible.constants.DEFAULT_LOCAL_TMP = '/tmp/ansible'
 ansible.constants.DEFAULT_REMOTE_TMP = '/tmp/ansible'
 ansible.local_tmp = '/tmp/ansible'
+SSM_CLIENT = boto3.client('ssm')
 
 # Set directories or ansible tries to set on it's own, which breaks in lambda
 def make_secret(secret):
@@ -25,16 +26,21 @@ def make_secret(secret):
     return [(DEFAULT_VAULT_ID_MATCH, VaultSecret(secret))]
 
 #TODO: Make this get the PW from SSM?
-def get_vault_password():
+def get_vault_password(key_name):
     """ gets vault password file and reutrns it cleaned"""
-    return "secretvaultpassword"
+    response = SSM_CLIENT.get_parameter(
+        Name=key_name,
+        WithDecryption=True
+    )
+    return response['Parameter']['Value']
 
 def lambda_handler(event, context):
     """Main Lambda function."""
-    vault_pass = get_vault_password()
+    vault_pass = get_vault_password(event['key_name'])
     logging.debug("Vault Password: %s", vault_pass)
     vault = VaultLib(make_secret(vault_pass))
-    secret=vault.encrypt("secretthing")
+    print("Secret to encrypt " + event["secret"])
+    secret=vault.encrypt(event["secret"])
     print(secret)
     LOGGER.debug(event)
     LOGGER.debug(context)
